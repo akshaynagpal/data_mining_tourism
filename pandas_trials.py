522deda5f3765dbbcbf551cda24e5e7a7f31e743
Akshay Nagpal <akshay2626@gmail.com>
2015-08-30 12:58:00 +0530

    python ananlysis trials

---
+++ pandas_trials.py
@@ -0,0 +1,16 @@
+
+## Pandas trials:
+# 
+# Get crime pattern in a state
+# Get tourism pattern in a state
+# Get total crimes patterns of 28 states 
+
+
+import pandas as pd
+import xlrd
+
+#x =  pd.read_csv('data\\CRIMES_STATEWISE_ 2013.csv')
+y =  pd.read_excel('data\\ROOMS_IN_DIFFERENT_CATEGORY_OF_HOTELS_IN_INDIA.xls','State-wise 2009-10',na_values=['NA'])
+
+
+ 
